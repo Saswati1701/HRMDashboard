b5d6210874a1d89c9b92ce52de8da21640dcbfe6
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2023-07-15 17:32:42 +0530

    updated colors of nav links on click

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,16 +7,13 @@ import { BsFillPeopleFill } from "react-icons/bs";
 import { MdAccountTree } from "react-icons/md";
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-const Route = ({component: Icons,name }) =>{
-  const handleClick = () => {
-
-  }
+const Route = ({component: Icons,name, selected=false, onClick }) =>{
   
     return(
-      <Link to={`/${name}`} style={{textDecoration:"none"}}>
-        <div className='eachRoute' onClick={handleClick}>
+      <Link to={`/${name}`} style={{textDecoration:"none", color: 'blue'}}>
+        <div className='eachRoute' onClick={onClick}>
             <Icons color="#C4C4C4" />
-            <span style={{color:"#C4C4C4", paddingLeft:"1rem"}}>{name}</span>
+            <span style={{color: selected ? 'blue' : '#C4C4C4' , paddingLeft:"1rem"}}>{name}</span>
         </div>
       </Link>
     )
@@ -25,16 +22,22 @@ const Route = ({component: Icons,name }) =>{
 
 
 const Navbar = () => {
+  const [select, setSelect] = useState("Dashboard")
+
+  const handleNavClick = (name) => {
+    setSelect(name)
+  }
+  
   return (
     <div className='left-container'>
       <div className='navbar-container'>
         <div className='navbar'>
-          <Route component={IoGrid} name="Dashboard"/>
-          <Route component={FaCalendarAlt} name="Calendar"/>
-          <Route component={PiHandbagSimpleFill} name="Jobs" />
-          <Route component={BsFillPeopleFill} name="Candidates"/>
-          <Route component={MdAccountTree} name="Structure"/>
-          <Route component={IoSettings} name="Settings"/>
+          <Route component={IoGrid} onClick={() => { handleNavClick("Dashboard") }} selected={ select === "Dashboard" ? true : false } name="Dashboard"/>
+          <Route component={FaCalendarAlt} onClick={() => { handleNavClick("Calendar") }} selected={ select === "Calendar" ? true : false } name="Calendar"/>
+          <Route component={PiHandbagSimpleFill} onClick={() => { handleNavClick("Jobs") }} selected={ select === "Jobs" ? true : false } name="Jobs" />
+          <Route component={BsFillPeopleFill} onClick={() => { handleNavClick("Candidates") }} selected={ select === "Candidates" ? true : false } name="Candidates"/>
+          <Route component={MdAccountTree} onClick={() => { handleNavClick("Structure") }} selected={ select === "Structure" ? true : false } name="Structure"/>
+          <Route component={IoSettings} onClick={() => { handleNavClick("Settings") }} selected={ select === "Settings" ? true : false } name="Settings"/>
         </div>
       </div>
     </div>
