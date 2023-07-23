6074a4d7e6c13c7ea9857b44aa3d00a1e399944b
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2023-07-23 21:59:24 +0530

    circular progress

--- src/components/insightcard/Insightcard.jsx
+++ src/components/insightcard/Insightcard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './insightcard.css'
 import { AiOutlineRise } from "react-icons/ai";
 import { PieChart } from '@mui/x-charts/PieChart';
-
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 // const theme = createTheme({
 //   overrides: {
@@ -31,6 +32,7 @@ const data2 = [
 
 
 const Insightcard = ({text, number, iconColor, iconbg, increase, image, color}) => {
+  const percentage = 70;
   return (
     <div className='insightcard-component'>
             <div className='insightcard-left-container'>
@@ -39,21 +41,35 @@ const Insightcard = ({text, number, iconColor, iconbg, increase, image, color}) 
                 <p style={{margin:"0", display:"flex", alignItems:"center", fontSize:"13px"}}><span className='icon-background' style={{margin:"0 0.5rem 0 0", backgroundColor:`${iconbg}`}} ><AiOutlineRise color={iconColor}/></span>{increase}</p>
             </div>
             <div className='insightcard-right-container'>
-              <img src={image} height="90px" alt="" srcset="" />
-            {/* <PieChart
-              series={[
-                
-                {
-                  data: data2,
-                  cx: 100,
-                  cy: 200,
-                  innerRadius: 40,
-                  outerRadius: 80,
-                },
-              ]}
-              height={300}
-              legend={{ hidden: true }}
-            /> */}
+            <div className="progress">
+              <CircularProgressbar 
+                value={percentage} 
+                text={`${percentage}%`}
+                styles={buildStyles({
+                  // Rotation of path and trail, in number of turns (0-1)
+                  rotation: 0.25,
+              
+                  // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                  strokeLinecap: 'butt',
+              
+                  // Text size
+                  textSize: '16px',
+              
+                  // How long animation takes to go from one percentage to another, in seconds
+                  pathTransitionDuration: 0.5,
+              
+                  // Can specify path transition in more detail, or remove it entirely
+                  // pathTransition: 'none',
+              
+                  // Colors
+                  pathColor: `${color}`,
+                  textColor: 'blue',
+                  trailColor: '#d6d6d6',
+                  backgroundColor: '#3e98c7',
+                })} 
+              />;
+            </div>
+              
             </div>
     </div>
   )
--- src/containers/xtra/Extra.jsx
+++ src/containers/xtra/Extra.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import './extra.css'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Extra = () => {
     const percentage = 70;
   return (
-    <div>
+    <div className='content'>
+      ass
+      <div className="progress">
         <CircularProgressbar 
-            value={percentage} 
-            text={`${percentage}%`} 
-            styles={buildStyles({
-                // Rotation of path and trail, in number of turns (0-1)
-                rotation: 0.25,
-            
-                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                strokeLinecap: 'butt',
-            
-                // Text size
-                textSize: '16px',
-            
-                // How long animation takes to go from one percentage to another, in seconds
-                pathTransitionDuration: 0.5,
-            
-                // Can specify path transition in more detail, or remove it entirely
-                // pathTransition: 'none',
-            
-                // Colors
-                pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
-                textColor: '#f88',
-                trailColor: '#d6d6d6',
-                backgroundColor: '#3e98c7',
-              })}
+          value={percentage} 
+          text={`${percentage}%`}
+          styles={buildStyles({
+            // Rotation of path and trail, in number of turns (0-1)
+            rotation: 0.25,
+        
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'butt',
+        
+            // Text size
+            textSize: '16px',
+        
+            // How long animation takes to go from one percentage to another, in seconds
+            pathTransitionDuration: 0.5,
+        
+            // Can specify path transition in more detail, or remove it entirely
+            // pathTransition: 'none',
+        
+            // Colors
+            pathColor: `red`,
+            textColor: 'blue',
+            trailColor: '#d6d6d6',
+            backgroundColor: 'red',
+          })} 
         />;
-
+      </div>
+        
     </div>
   )
 }
