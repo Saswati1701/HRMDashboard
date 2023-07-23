9ef2df2ccd4caed3375c30771f3561a3e91ad170
Saswati1701 <saswatiisabella2002@gmail.com>
2023-07-23 17:13:05 +0530

    topbar changes

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,8 +6,8 @@ const Topbar = () => {
   return (
     <div className='topbar'>
       <div className='search-bar'>
-        <input type="text" placeholder="Search for candidates" />
-        <div style={{backgroundColor:"blue"}}><RiSearchLine color='white'/> </div>
+        <input type="text" placeholder="Search for candidates" style={{border:'none', padding:'1rem' , borderRadius:'0.5rem 0 0 0.5rem',  backgroundColor:"#FAFBFD  "}} />
+        <RiSearchLine color='white' size={30} style={{ backgroundColor:"blue", borderRadius:'0 0.5rem 0.5rem 0', padding:'0.5rem'}}/>
       </div>
       <div className="profile">
         <div className='circles'></div>
