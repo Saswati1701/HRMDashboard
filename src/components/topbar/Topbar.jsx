ca86de9195b0b5070007e0375e0eb5c640f0bf27
Saswati1701 <saswatiisabella2002@gmail.com>
2023-07-23 20:46:51 +0530

    topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,14 +5,16 @@ import { RiSearchLine } from "react-icons/ri";
 const Topbar = () => {
   return (
     <div className='topbar'>
-      <div className='search-bar'>
-        <input type="text" placeholder="Search for candidates" style={{border:'none', padding:'1rem' , borderRadius:'0.5rem 0 0 0.5rem',  backgroundColor:"#FAFBFD  "}} />
-        <RiSearchLine color='white' size={30} style={{ backgroundColor:"blue", borderRadius:'0 0.5rem 0.5rem 0', padding:'0.5rem'}}/>
-      </div>
-      <div className="profile">
-        <div className='circles'></div>
-        <div className='circles'></div>
-        <div className='circles'></div>
+      <div className='place-search-profile'>
+        <div className='search-bar'>
+          <input type="text" placeholder="Search for candidates" style={{border:'none', padding:'1rem' , borderRadius:'0.5rem 0 0 0.5rem',  backgroundColor:"#FAFBFD  "}} />
+          <RiSearchLine color='white' size={30} style={{ backgroundColor:"blue", borderRadius:'0 0.5rem 0.5rem 0', padding:'0.5rem'}}/>
+        </div>
+        <div className="profile">
+          <div className='circles'></div>
+          <div className='circles'></div>
+          <div className='circles'></div>
+        </div>
       </div>
         
     </div>
