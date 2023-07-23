8b662ede1cdccad560a7ff17a968914266e8f74c
Saswati1701 <saswatiisabella2002@gmail.com>
2023-07-23 22:55:50 +0530

    topbar fixed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,7 @@ import './topbar.css'
 import { RiSearchLine } from "react-icons/ri";
 import { BsQuestionCircleFill } from "react-icons/bs";
 import { BiSolidBellRing } from "react-icons/bi";
+import { Avatar } from '@mui/material';
 
 
 const Topbar = () => {
@@ -14,9 +15,9 @@ const Topbar = () => {
           <RiSearchLine color='white' size={30} style={{ backgroundColor:"#377DFF", borderRadius:'0 0.5rem 0.5rem 0', padding:'0.5rem'}}/>
         </div>
         <div className="profile">
-          <BsQuestionCircleFill/>
-          <BiSolidBellRing/>
-          <div className='circles'></div>
+          <BsQuestionCircleFill size={25}/>
+          <BiSolidBellRing size={25}/>
+          <Avatar sx={{height:'29px', width:"29px"}} />
         </div>
       </div>
         
