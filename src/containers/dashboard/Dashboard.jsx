9c9f60507f5aac03ea1131259d4e8545a24ff3ee
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2023-07-24 22:38:22 +0530

    bar chart done

--- src/containers/dashboard/Dashboard.jsx
+++ src/containers/dashboard/Dashboard.jsx
@@ -5,13 +5,71 @@ import GreenChart from '../../images/circle1.png'
 import YellowChart from '../../images/circle2.png'
 import OrangeChart from '../../images/circle3.png'
 import './dashboard.css'
-import ideal from '../../images/ideal.png'
-import graph from '../../images/graph.png'
-import { alpha, styled } from '@mui/material/styles';
-import { pink } from '@mui/material/colors';
 import Switch_component from '../../components/switch/Switch'
 import Dropdown from '../../components/dropdown/Dropdown'
 import ActivityFeed from '../../components/activityFeed/ActivityFeed'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      // barPercentage: 0.3,
+      stacked: true,
+    },
+    y: {
+      // barPercentage: 0.3,
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Applied',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: 'Shortlisted',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(75, 192, 192)',
+    },
+    {
+      label: 'Rejected',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(53, 162, 235)',
+    },
+  ],
+};
 
 const Dashboard = () => {
   return (
@@ -64,7 +122,10 @@ const Dashboard = () => {
               <Dropdown val="Month" />
             </div>
           </div>
-          <img src={graph} height="250px" alt="" />
+          
+          <div className="graph">
+            <Bar options={options} data={data} />
+          </div>
         </div>
 
         <div className='bottom-dashboard'>
--- src/containers/xtra/Extra.jsx
+++ src/containers/xtra/Extra.jsx
@@ -1,40 +1,75 @@
 import React from 'react'
 import './extra.css'
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      // barPercentage: 0.3,
+      stacked: true,
+    },
+    y: {
+      // barPercentage: 0.3,
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Applied',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: 'Shortlisted',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(75, 192, 192)',
+    },
+    {
+      label: 'Rejected',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      backgroundColor: 'rgb(53, 162, 235)',
+    },
+  ],
+};
+
 
 const Extra = () => {
-    const percentage = 70;
   return (
     <div className='content'>
       ass
-      <div className="progress">
-        <CircularProgressbar 
-          value={percentage} 
-          text={`${percentage}%`}
-          styles={buildStyles({
-            // Rotation of path and trail, in number of turns (0-1)
-            rotation: 0.25,
-        
-            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-            strokeLinecap: 'butt',
-        
-            // Text size
-            textSize: '16px',
-        
-            // How long animation takes to go from one percentage to another, in seconds
-            pathTransitionDuration: 0.5,
-        
-            // Can specify path transition in more detail, or remove it entirely
-            // pathTransition: 'none',
-        
-            // Colors
-            pathColor: `red`,
-            textColor: 'blue',
-            trailColor: '#d6d6d6',
-            backgroundColor: 'red',
-          })} 
-        />;
+      <div className="chart">
+      <Bar options={options} data={data} />
       </div>
         
     </div>
