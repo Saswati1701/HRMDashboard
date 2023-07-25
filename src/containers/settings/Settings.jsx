954ec33357536561c4cb506491ddc32c43caf223
Saswati1701 <saswatiisabella2002@gmail.com>
2023-07-25 22:28:18 +0530

    settings changes

--- src/containers/settings/Settings.jsx
+++ src/containers/settings/Settings.jsx
@@ -1,15 +1,66 @@
 import React from 'react'
 import Topbar from '../../components/topbar/Topbar'
+import './settings.css'
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Settings = () => {
   return (
     <div style={{width:"100%", marginLeft:'250px'}}>
       <Topbar/>
-      <div>
-        Settings
+      <div className='settings-container'>
+        <div className='settings-profile'>
+          <div className='profile-circle'></div>
+          <div style={{marginLeft:'2rem'}}>
+            <p>Saswati Mahanta</p>
+            <p>Head of Recruiting</p>
+            <p>Odisha, India</p>
+          </div>
+        </div>
+        <div className='personal-info'>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div className='input-box'>
+            <TextField
+              required
+              // id="outlined-required"
+              label="First Name"
+              defaultValue="Saswati Mahanta"
+            />
+            <TextField
+              required
+              // id="outlined-required"
+              label="First Name"
+              defaultValue="Saswati Mahanta"
+            />
+            <TextField
+              required
+              // id="outlined-required"
+              label="First Name"
+              defaultValue="Saswati Mahanta"
+            />
+            <TextField
+              required
+              // id="outlined-required"
+              label="First Name"
+              defaultValue="Saswati Mahanta"
+            />
+            
+          
+          </div>
+        </Box>
+        </div>
+        <div className='job-portal'></div>
       </div>
     </div>
-  )
+  )   
 }
 
 export default Settings
