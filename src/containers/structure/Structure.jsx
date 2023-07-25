1a274808c7350c36817da6b74e1cec409b66920a
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2023-07-25 22:28:21 +0530

    structure page

--- src/containers/structure/Structure.jsx
+++ src/containers/structure/Structure.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
-import Topbar from '../../components/topbar/Topbar'
+import './structure.css'
 
 const Structure = () => {
   return (
-    <div style={{width:"100%", marginLeft:'250px'}}>
-      <Topbar/>
-      <div>
-        Structure
+    <div className='strContainer'>
+      <div className="strRow one">
+        <div className="strCol one">
+          1.
+          Board of Directors
+          <div className="names">
+            <div className="name">Ravi</div>
+            <div className="name">Rahul</div>
+            <div className="name">Ramesh</div>
+          </div>
+        </div>
+        <div className="strCol two">
+          2.
+          CEO
+          <div className="names">
+            <div className="name">Ravi</div>
+            <div className="name">Rahul</div>
+            <div className="name">Ramesh</div>
+          </div>
+        </div>
+      </div>
+      <div className="strRow two">
+        <div className="strCol three">
+          4.
+          Directors
+          <div className="names">
+            <div className="name">Ravi</div>
+            <div className="name">Rahul</div>
+            <div className="name">Ramesh</div>
+          </div>
+        </div>
+        <div className="strCol four">
+          3.
+          Vice President
+          <div className="names">
+            <div className="name">Ravi</div>
+            <div className="name">Rahul</div>
+            <div className="name">Ramesh</div>
+          </div>
+        </div>
       </div>
     </div>
   )
